41bb1013e09063e79b26012d57598389d97f966f
Yaki <shibatales@gmail.com>
2023-08-18 20:47:54 -0700

    chore: page icons wip

--- src/components/left-side/SidenavLeft.tsx
+++ src/components/left-side/SidenavLeft.tsx
@@ -1,10 +1,10 @@
 import { A } from "@solidjs/router";
-import { For } from "solid-js";
+import { For, JSXElement } from "solid-js";
 import styles from '../../App.module.css';
-import AssetsIcon from '../../assets/icons/assets-icon-15x12.svg';
-import TransactionsIcon from '../../assets/icons/transactions-icon-15x12.svg';
-import MembersIcon from '../../assets/icons/members-icon-15x15.svg';
-import SettingsIcon from '../../assets/icons/settings-icon-15x15.svg';
+import { AssetsIcon } from '../../components/svg-icons/AssetsIcon';
+import { TransactionsIcon } from '../../components/svg-icons/TransactionsIcon';
+import { MembersIcon } from '../../components/svg-icons/MembersIcon';
+import { SettingsIcon } from '../../components/svg-icons/SettingsIcon';
 import ExternalLinkIcon from '../../assets/icons/external-link-icon-16x16.svg';
 import AddMultisigIcon from '../../assets/icons/add-multisig-icon-15x15.svg';
 import ColorSwitch from "./ColorSwitch";
@@ -16,25 +16,26 @@ const pages = [
   "settings",
 ];
 
-function matchIconToPage(page: string): string {
+function matchIconToPage(page: string): JSXElement {
+  // Assign the correct icon component to each page link
   switch (page) {
     case "assets":
-      return AssetsIcon;
+      return <AssetsIcon />;
     case "transactions":
-      return TransactionsIcon;
+      return <TransactionsIcon />;
     case "members":
-      return MembersIcon;
+      return <MembersIcon />;
     case "settings":
-      return SettingsIcon;
+      return <SettingsIcon />;
     default:
-      return SettingsIcon;
+      return null;
   }
 }
 
 const SidenavLeft = () => {
-  return <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-72 h-screen mt-24 transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+  return <aside id="left-sidebar" class="fixed top-0 left-0 z-40 w-72 h-screen mt-24 transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
     <div class="h-full px-3 py-4 px-5 overflow-y-auto bg-saturn-offwhite dark:bg-saturn-black">
-      <div class="grid grid-rows-3 grid-cols-1">
+      <div class="grid grid-cols-1 gap-12">
         {/* pages */}
         <div class={styles.pageListContainer}>
           <For each={pages}>
@@ -46,17 +47,17 @@ const SidenavLeft = () => {
               >
                 <div class={styles.selectedItemGradient} />
                 <div class={styles.selectedItemIndicator} />
-                <img src={matchIconToPage(page)} alt={`${ page }-icon`} class="pageIcon" />
-                <span class="ml-4">{page}</span>
+                {matchIconToPage(page)}
+                <span class="ml-4 text-saturn-black dark:text-saturn-offwhite">{page}</span>
               </A>
             )}
           </For>
         </div>
 
         <div>
-          {/* add new multisig */}
-          <button class="bg-saturn-purple hover:bg-purple-800 text-xs p-5 mb-5 w-full rounded-md flex justify-center items-center">
-            <img src={AddMultisigIcon} alt="add-multisig-icon" width={10} height={10} class="mr-2" />
+          {/* add new multisig button */}
+          <button type="button" class="bg-saturn-purple hover:bg-purple-800 text-xs p-5 mb-5 w-full rounded-md flex justify-center items-center">
+            <img src={AddMultisigIcon} alt="add-multisig-icon" width={12} height={12} class="mr-2" />
             <span>Add Multisig</span>
           </button>
 
@@ -88,6 +89,7 @@ const SidenavLeft = () => {
             </li>
           </ul>
 
+          {/* theme switcher */}
           <div class="mt-4">
             <ColorSwitch />
           </div>
