b6786eebc33cf789e72bb2e718006958bad6ac68
Yaki <shibatales@gmail.com>
2023-09-08 18:43:55 -0700

    chore: smol style nit

--- src/components/right-side/ActivityRow.tsx
+++ src/components/right-side/ActivityRow.tsx
@@ -5,19 +5,19 @@ type ActivityRowProps = {
 };
 
 const ActivityRow = (props: ActivityRowProps) => {
-  return <div class="flex flex-col w-[313.65px] border-b border-px border-gray-800 pb-3 mb-3">
+  return <div class="flex flex-col w-[313.65px] border-b border-px dark:border-gray-800 border-gray-100 pb-3 mb-3">
     <div class="flex flex-row mb-1">
       <div class="text-saturn-lightgrey text-xxs flex flex-row justify-between w-full">
         <span>{props.timestamp}</span>
         <span class="float-right">
-          <span>Voted {' '}</span>
-          <span class={`${ props.aye ? 'text-saturn-green' : 'text-saturn-red' }`}>
+          <span>Voted</span>
+          <span class={`p-1 ml-[5px] rounded ${ props.aye ? 'bg-saturn-green text-black' : 'bg-saturn-red text-white' }`}>
             {props.aye ? 'Aye' : 'Nay'}
           </span>
         </span>
       </div>
     </div>
-    <span class="text-xs">Executed {props.activity} call</span>
+    <span class="text-xs text-black dark:text-white">Executed {props.activity} call</span>
   </div>;
 };
 
