33c0ea8597c4a48d59bc47fc53bcd1f43181dc53
Yaki <shibatales@gmail.com>
2024-02-15 19:54:04 -0800

    Refactor getBalancesFromAllNetworks function

--- src/utils/getBalances.ts
+++ src/utils/getBalances.ts
@@ -21,33 +21,35 @@ export type NetworkBalances = {
 export async function getBalancesFromAllNetworks(address: string): Promise<NetworkBalances> {
   return fetch(
     PolkaholicStartUrl + address + AllChainsUrl + "&decrate=f",
-  ).then(async response => response.json().then(res => {
-    const newRes = (res as { state: { free_raw?: string, reserved_raw?: string, frozen_raw?: string, id: string, symbol: string; }; }[]);
+  ).then(async response => {
+    return response.json().then(res => {
+      const newRes = (res as { state: { free_raw?: string, reserved_raw?: string, frozen_raw?: string, id: string, symbol: string; }; }[]);
 
-    const balances: NetworkBalances = Array.isArray(newRes) ? newRes
-      .map((asset) => {
-        return {
-          [asset.state.id as keyof typeof Rings]: {
-            [asset.state.symbol]: {
-              freeBalance: asset.state.free_raw || "0",
-              reservedBalance: asset.state.reserved_raw || "0",
-              frozenBalance: asset.state.frozen_raw || "0",
+      const balances: NetworkBalances = Array.isArray(newRes) ? newRes
+        .map((asset) => {
+          return {
+            [asset.state.id as keyof typeof Rings]: {
+              [asset.state.symbol]: {
+                freeBalance: asset.state.free_raw || "0",
+                reservedBalance: asset.state.reserved_raw || "0",
+                frozenBalance: asset.state.frozen_raw || "0",
+              }
             }
-          }
-        } as NetworkBalances;
-      })
-      .reduce((prev: NetworkBalances, asset) => {
-        let newPrev = prev;
-        let assets = prev[Object.keys(asset)[0] as keyof typeof Rings] || {};
+          } as NetworkBalances;
+        })
+        .reduce((prev: NetworkBalances, asset) => {
+          let newPrev = prev;
+          let assets = prev[Object.keys(asset)[0] as keyof typeof Rings] || {};
 
-        assets[Object.keys(Object.values(asset)[0])[0]] = Object.values(Object.values(asset)[0])[0];
+          assets[Object.keys(Object.values(asset)[0])[0]] = Object.values(Object.values(asset)[0])[0];
 
-        newPrev[Object.keys(asset)[0] as keyof typeof Rings] = assets;
+          newPrev[Object.keys(asset)[0] as keyof typeof Rings] = assets;
 
-        return newPrev;
-      }, {} as NetworkBalances) : {} as NetworkBalances;
+          return newPrev;
+        }, {} as NetworkBalances) : {} as NetworkBalances;
 
-    return balances;
-  }
-  ));
+      return balances;
+    });
+  });
 }
+
