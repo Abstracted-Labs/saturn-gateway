c25613c93d90d5e1a2c5a94fbc8bc44506286b00
Yaki <shibatales@gmail.com>
2024-03-11 09:08:52 -0700

    Update LeftContent styling

--- src/components/left-side/LeftContent.tsx
+++ src/components/left-side/LeftContent.tsx
@@ -6,7 +6,7 @@ import { Show } from "solid-js";
 import PageLinks from "./PageLinks";
 
 const LeftContent = (props: { inDrawer: boolean; }) => {
-  return <div class={`h-[87%] px-5 pt-4 pb-10 lg:pb-2 flex flex-col bg-saturn-offwhite dark:bg-saturn-black ${ props.inDrawer ? 'overflow-y-hidden' : 'mr-4 overflow-y-auto saturn-scrollbar' }`}
+  return <div class={`h-[87%] px-5 pt-1 pb-10 lg:pb-2 flex flex-col bg-saturn-offwhite dark:bg-saturn-black ${ props.inDrawer ? 'overflow-y-hidden' : 'mr-4 overflow-y-auto saturn-scrollbar' }`}
   >
     <Show when={props.inDrawer}>
       <span id="inDrawer" />
